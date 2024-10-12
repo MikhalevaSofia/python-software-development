8e466116893412eb6ea4be1a2d96ced4233357aa
MikhalevaSofia <100435644+MikhalevaSofia@users.noreply.github.com>
2024-10-12 10:55:50 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task2_1.py
@@ -0,0 +1,12 @@
+# TODO Найдите количество книг, которое можно разместить на дискете
+VALUE = 1.44 # Объем дискеты в Мб
+PAGES = 100
+LINES_ON_PAGE = 50
+SYMBOLS_IN_LINE = 25
+VALUE_FOR_ONE_SYMBOL = 4 # Объем для хранения одного символа в байтах
+
+value_for_book = SYMBOLS_IN_LINE * LINES_ON_PAGE * PAGES * VALUE_FOR_ONE_SYMBOL
+total_books = int(VALUE * 1024 ** 2// value_for_book)
+
+print("Количество книг, помещающихся на дискету:", total_books)
+
