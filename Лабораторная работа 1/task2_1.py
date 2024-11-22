91ccf87397dcb2dc689c280a227a8f5f1f888797
MikhalevaSofia <100435644+MikhalevaSofia@users.noreply.github.com>
2024-11-22 11:56:00 +0300

    Update task2_1.py

--- Лабораторная работа 1/task2_1.py
+++ Лабораторная работа 1/task2_1.py
@@ -6,7 +6,7 @@ SYMBOLS_IN_LINE = 25
 VALUE_FOR_ONE_SYMBOL = 4 # Объем для хранения одного символа в байтах
 
 value_for_book = SYMBOLS_IN_LINE * LINES_ON_PAGE * PAGES * VALUE_FOR_ONE_SYMBOL
-total_books = int(VALUE * 1024 ** 2// value_for_book)
+total_books = int(VALUE * 1024 ** 2 / value_for_book)
 
 print("Количество книг, помещающихся на дискету:", total_books)
 
